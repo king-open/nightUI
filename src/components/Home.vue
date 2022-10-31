6bb90da49f70ea1ed4658bcb1e7388ce3e89a3fd
king-open <kingwang.0624@gmail.com>
2022-10-31 16:24:33 +0800

    初始化 aside

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,8 +5,8 @@
       <h1>Night UI</h1>
       <h2>一个很厉害的UI的框架</h2>
       <p class="actions">
-        <a href="">Github</a>
-        <a href="">开始</a>
+        <a href="https://gihub.com">Github</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
