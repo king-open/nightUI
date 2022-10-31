3e12c39e2c28b4f0f6328c5a529dc987d851af04
king-open <kingwang.0624@gmail.com>
2022-10-31 19:53:07 +0800

    优化css

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -26,42 +26,18 @@ export default {
 @use "sass:math";
 $h: 22px;
 $h2: $h - 4px;
-button {
-  height: $h;
-  width: $h * 2;
-  border: none;
-  background: #bfbfbf;
-  border-radius: $h/2;
-  position: relative;
-  transition: all 250ms;
-}
-span {
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  height: $h2;
-  width: $h2;
-  background: white;
-  border-radius: $h2 / 2;
-}
-button.checked {
-  background: #6495ed;
-}
-button.checked > span {
-  left: calc(100% - #{$h2} - 2px);
-}
-button:focus {
-  outline: none;
-}
-button:active {
-  > span {
-    width: $h2 + 4px;
+button {height: $h;width: $h * 2;border: none;background: #bfbfbf;border-radius: $h/2;position: relative;
+  > span {position: absolute;top: 2px;left: 2px;height: $h2;width: $h2;background: white;border-radius: $h2 / 2;transition: all 250ms;
   }
-}
-button.checked:active {
-  > span {
-    width: $h2 + 4px;
-    margin-left: -4px;
+  &.checked {background: #1890ff;
+    > span {left: calc(100% - #{$h2} - 2px);}
+  }
+  &:focus {outline: none;}
+  &:active {
+    > span {width: $h2 + 4px;}
+  }
+  &.checked:active {
+    > span {width: $h2 + 4px;margin-left: -4px;}
   }
 }
 </style>
