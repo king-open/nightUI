7cfd5e2b6374a62dd79430858144f1043615353f
king-open <kingwang.0624@gmail.com>
2022-11-03 17:00:01 +0800

    遇到vue3 bug

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="n-tabs">
   <div class="n-tabs-nav">
-    <div class="n-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+    <div class="n-tabs-nav-item" v-for="(t,index) in titles" @click="select(t)" :class="{selected: t=== selected}"  :key="index">{{t}}</div>
   </div>
   <div class="n-tabs-content">
-    <component class="n-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index" />
+    {{current}}
+    <component class="n-tabs-content-item" :is="current" />
   </div>
   </div>
 </template>
 
 <script lang="ts"> 
+import { computed } from 'vue'
 import Tab from './Tab.vue'
 export default {
+  props: {
+    selected: {
+      type: String
+    }
+  },
   setup(props, context) {
     const defaults = context.slots.default()
     defaults.forEach((tag) => {
@@ -19,13 +26,24 @@ export default {
         throw new Error('Tabs 子标签必须是 Tab')
       }
     })
+    const current = computed(() => {
+      console.log('重新 return')
+      return defaults.filter((tag) => {
+        return tag.props.title === props.selected
+      })[0]
+    })
     const titles = defaults.map((tag) => {
       return tag.props.title
       console.log('tag.props')
     })
+    const select = (title: string) => {
+      context.emit('update:selected', title)
+    }
     return {
       defaults,
-      titles
+      titles,
+      current,
+      select
     }
   }
 }
