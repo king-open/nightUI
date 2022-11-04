0077c9ea902c54ae0a88a35146b7132113c6484b
king-open <kingwang.0624@gmail.com>
2022-11-04 16:30:07 +0800

    update:

--- README.md
+++ README.md
@@ -10,6 +10,11 @@
 
 所以如果强烈不建议你将此 UI 库用于生产环境。但如果你是抱着看源代码的目的来的，那么这个库还是值得一看的。源代码放在了 github上，历史提交信息非常规范，你可以按提交的顺序逐个查看；你也可以直接查看每个组件的源代码和示例，运行方法见 README.md。(已经发布npm(怕该名字)被别人抢了，就学**antfu** 在写一个项目好玩的东西前先发布npm) 还没写完
 
+## 更新日志
+
+v0.0.1 init 
+v0.0.2 更改 Tabs 组件  
+
 ## MIT 
 版权 (c) 2022-至今 归 King-open 所有. 详情请阅 [LICENSE](https://github.com/king-open/nightUI/blob/main/license)
 
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,48 +1,24 @@
 <template>
-  <div class="n-tabs">
-  <div class="n-tabs-nav">
-    <div class="n-tabs-nav-item" v-for="(t,index) in titles" @click="select(t)" :class="{selected: t=== selected}"  :key="index">{{t}}</div>
-  </div>
-  <div class="n-tabs-content">
-    <component class="n-tabs-content-item" :class="{selected: c.props.title === selected }" v-for="c in defaults" :is="c" />
-  </div>
-  </div>
+  <div v-for="(t,index) in titles" :key="index">{{t}}</div>
+  <component v-for="(c,index) in defaults" :is="c" :key="index" />
 </template>
 
-<script lang="ts"> 
-import { computed } from 'vue'
+<script lang="ts">
 import Tab from './Tab.vue'
 export default {
-  props: {
-    selected: {
-      type: String
-    }
-  },
-  setup(props, context) {
+  setup: function (props, context) {
     const defaults = context.slots.default()
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
         throw new Error('Tabs 子标签必须是 Tab')
       }
     })
-    const current = computed(() => {
-      console.log('重新 return')
-      return defaults.filter((tag) => {
-        return tag.props.title === props.selected
-      })[0]
-    })
     const titles = defaults.map((tag) => {
       return tag.props.title
-      console.log('tag.props')
     })
-    const select = (title: string) => {
-      context.emit('update:selected', title)
-    }
     return {
       defaults,
-      titles,
-      current,
-      select
+      titles
     }
   }
 }
