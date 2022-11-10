5833a7f50eaf94f861e13b9d38bb30fc03499acb
king-open <kingwang.0624@gmail.com>
2022-11-11 00:27:22 +0800

    优化1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="n-tabs">
     <div class="n-tabs-nav" ref="container">
-      <div class="n-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if (el) navItems[index] = el }" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
+      <div class="n-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if (t===selected) selectedItem = el }" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
       <div class="n-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="n-tabs-content">
@@ -21,17 +21,14 @@ export default {
     }
   },
   setup(props, context) {
-    const navItems = ref<HTMLDivElement[] > ([])
+    const selectedItem = ref<HTMLDivElement > (null)
     const indicator = ref <HTMLDivElement > (null)
     const container = ref <HTMLDivElement> (null)
     const x = ()=>{
-      const divs = navItems.value
-      const result = divs.filter(div => div.classList.contains('selected'))[0]
-      console.log(result)
-      const {width} = result.getBoundingClientRect()
+      const {width} = selectedItem.value.getBoundingClientRect()
       indicator.value.style.width = width + 'px'
       const {left:left1} = container.value.getBoundingClientRect()
-      const {left:left2} = result.getBoundingClientRect()
+      const {left:left2} = selectedItem.value.getBoundingClientRect()
       const left = left2 - left1
       indicator.value.style.left = left + 'px'
     }
@@ -59,7 +56,7 @@ export default {
       titles,
       current,
       select,
-      navItems,
+      selectedItem,
       indicator,
       container
     }
