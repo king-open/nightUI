799fab8d5e0b49b85c4cb6cf32a41cfe144f89d0
king-open <kingwang.0624@gmail.com>
2022-11-11 01:02:13 +0800

    使用本地的 svg.js

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import  './index.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
 import {router} from './router'
+import './lib/svg.js'
+
+
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
