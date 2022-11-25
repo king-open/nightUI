2e1ee5199075f346db16706bf562e19e555d8e85
king-open <kingwang.0624@gmail.com>
2022-11-25 12:35:44 +0800

    创建路由

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,14 @@
 import  { createRouter, createWebHashHistory } from 'vue-router'
 import Home from './components/Home.vue'
-import Doc from './components/Doc.vue'
+import Doc from './views/Doc.vue'
 import  SwitchDemo from './components/SwitchDemo.vue'
 import DocDemo from './components/DocDemo.vue'
 import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
-
+import Intro from './views/Intro.vue'
+import GetStarted from './views/GetStarted.vue'
+import Install from './views/Install.vue'
 
 const history =  createWebHashHistory()
 export const router = createRouter({
@@ -18,6 +20,9 @@ export const router = createRouter({
       component:Doc,
       children: [
         { path: "", component: DocDemo },
+        { path: "intro", component: Intro },
+        { path: "get-started", component: GetStarted },
+        { path: "install", component: Install },
         { path: 'switch', component: SwitchDemo },
         { path: 'button', component: ButtonDemo },
         { path: 'dialog', component: DialogDemo },
